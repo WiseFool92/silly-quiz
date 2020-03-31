741ed2199722b358f29d1297221ea98a2933664b
mai cook <mailiecook@yahoo.com>
2020-03-31 16:18:08 -0700

    add background imgs in styles.css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,89 @@ $(document).ready(function() {
   $("form#checkbox").submit(function(event) {
     event.preventDefault();
     var firstQuestion = $("input:checkbox[name=q1]:checked").val();
+    var secondQuestion = $("input:checkbox[name=q2]:checked").val();
+    // var 
+    // var results = 
     console.log(firstQuestion);
   });
 });
+
+// $(document).ready(function(){
+//   $("form#radio").submit(function(event){
+ 
+//    event.preventDefault();
+   
+//    var question1 = $("input:radio[name=q1]:checked").val();
+//    var question2 = $("input:radio[name=q2]:checked").val();
+//    var question3 = $("input:radio[name=q3]:checked").val();
+//    var question4 = $("input:radio[name=q4]:checked").val();
+//    var question5 = $("input:radio[name=q5]:checked").val();
+//    var aResults = 0;
+//    var bResults = 0;
+//    var cResults = 0;
+  
+   
+ 
+//    var conclusionsArray = [question1,question2,question3,question4,question5]
+//    console.log(conclusionsArray);
+//    for (let i = 0; i < 5; i++) {
+   
+//      if (conclusionsArray[i] === "a") {
+//        aResults += 1;
+//      }
+//      else if (conclusionsArray[i] === "b") {
+//        bResults += 1;
+//      }
+//      else if (conclusionsArray[i] === "c") {
+//        cResults += 1;
+//      }
+//    }
+ 
+//    var preliminaryWinner = "a";
+ 
+//    if (bResults >= aResults) {  
+//      preliminaryWinner = "b";
+//    }
+//    if (cResults >= aResults) {
+//      preliminaryWinner = "c"
+//    }
+//    if (bResults >= cResults && bResults >= aResults) {
+//      preliminaryWinner = "b"
+//      }
+     
+  
+//      if (preliminaryWinner === "a") {
+       
+//        $("#a-victor").removeClass("hidden");
+//      }
+//      else if (preliminaryWinner === "b") {
+//        $("#b-victor").removeClass();
+//      }
+//      else if (preliminaryWinner === "c") {
+//        $("#c-victor").removeClass();
+//      }
+  
+//    });
+//  });
+ 
+//  // https://www.dyn-web.com/tutorials/forms/radio/get-selected.php
+ 
+ 
+//  //function howMany(selectObject) {
+//  //  let numberSelected = 0;
+//  //  for (let i = 0; i < selectObject.options.length; i++) {
+//  //    if (selectObject.options[i].selected) {
+//  //      numberSelected++;
+//  //    }
+//  //  }
+//  //  return numberSelected;
+//  // }
+ 
+ 
+//  // if (question1 === "a") {
+//   //   var result = add(number1, number2);
+ 
+//  // var arr = [1,2,3]
+//  // var max = arr.reduce(function(a, b, c) {
+//  //  return Math.max(a, b, c);
+//  // });
